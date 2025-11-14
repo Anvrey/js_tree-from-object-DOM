d1af5924d959976e97a53b5bbb8786f63770685e
Andrey <inokatre08@gmail.com>
2025-11-15 00:28:13 +0200

    make build

---
+++ index.952e3060.js
@@ -0,0 +1,2 @@
+!function e(n,r){var t=document.createElement("ul");for(var a in r){var c=document.createElement("li");c.textContent=a,e(c,r[a]),t.append(c)}n.append(t)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.952e3060.js.map
